a61d73a0d2beee42b9bda14648c8d46288059cfb
Malte Renken <malte.renken@fu-berlin.de>
2016-05-28 21:18:58 +0200

    Added finding strongly connected components.

--- mcmm/analysis.py
+++ mcmm/analysis.py
@@ -4,11 +4,24 @@ This module should handle the analysis of an estimated Markov state model.
 
 import numpy as np
 
+class Error(Exception):
+    """Base class for all exceptions raised by the mcmm module."""
+
+class InvalidOperation(Error):
+    """An operation was called on a object that does not support it."""
+
 class MarkovStateModel:
 
     def __init__(self, transition_matrix):
         self._transition_matrix = transition_matrix
         self._stationary_distribution = None
+        self._is_irreducible = None
+
+    @property
+    def is_irreducible(self):
+        if self._is_irreducible is None:
+            self._is_irreducible = (len(strongly_connected_components(self.transition_matrix)) == 1)
+        return self._is_irreducible
 
     @property
     def transition_matrix(self):
@@ -24,6 +37,8 @@ class MarkovStateModel:
         """Finds the stationary distribution of a given stochastic matrix.
         The matrix is assumed to be irreducible.
         """
+        if not self.is_irreducible:
+            raise InvalidOperation('Cannot compute stationary distribution of reducible markov chain')
         eigenvalues, eigenvectors = np.linalg.eig(self.transition_matrix.T)
         norms = [np.absolute(v) for v in eigenvalues]
         i = np.argmax(norms)
@@ -31,4 +46,46 @@ class MarkovStateModel:
         return eigenvectors[:,i]
 
 
+def depth_first_search(adjacency_matrix, root, flags):
+    """Performs depth-first search on a digraph.
+    
+    Parameters:
+    adjacency_matrix: numpy.ndarray of shape (n, n) containing node-node adjancencies.
+    root: Root node
+    flags: List of vertex flags. All vertices whose flag is initially set are ignored. After return the flags of all found vertices will be set.
+    
+    Returns a list of all nodes reachable from root sorted by
+    post-order traversal.
+    """
+    result = []
+    flags[root] = True
+    for vertex in range(adjacency_matrix.shape[0]):
+        if adjacency_matrix[root,vertex]:
+            if not flags[vertex]:
+                result += depth_first_search(adjacency_matrix, vertex, flags)
+    result.append(root)
+    return result
 
+
+def strongly_connected_components(adjacency_matrix):
+    """Finds all strongly connected components of a digraph.
+    
+    Parameters:
+    adjacency_matrix: numpy.ndarray of shape (n, n) containing node-node adjancencies.
+    
+    Returns a list of strongly connected components, each of which is a list of vertices.
+    """
+    nodes = range(adjacency_matrix.shape[0])
+    flags = [False] * len(nodes)
+    node_list = []
+    for node in nodes:
+        if not flags[node]:
+            node_list += depth_first_search(adjacency_matrix, node, flags)
+    for n in nodes:
+        assert(flags[n])
+    flags = [False] * len(nodes)
+    components = []
+    for node in reversed(node_list):
+        if not flags[node]:
+            components.append(depth_first_search(adjacency_matrix.T, node, flags))
+    return components
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -1,6 +1,7 @@
 from mcmm import analysis as ana
 import numpy as np
-from nose.tools import assert_true
+import random
+from nose.tools import assert_true, assert_equals, assert_raises
 
 def generate_random_stochastic_matrix():
     matrix = np.random.rand(4,4)+0.001
@@ -16,3 +17,29 @@ def test_find_stationary_distribution():
     np.testing.assert_array_almost_equal(np.dot(matrix.T, distrib), distrib)
     np.testing.assert_almost_equal(np.linalg.norm(distrib), 1)
 
+def test_find_stationary_distribution_raises():
+    matrix = np.zeros(shape=(3, 3))
+    msm = ana.MarkovStateModel(matrix)
+    with assert_raises(ana.InvalidOperation):
+        distrib = msm.stationary_distribution
+
+def test_depth_first_search():
+    nodes = 10
+    matrix = np.random.randint(2, size=(nodes,nodes))
+    root = random.randrange(0, nodes)
+    flags = [False]*nodes
+    result = ana.depth_first_search(matrix, root, flags)
+    assert_equals(root, result[-1])
+    for v1 in result:
+        for v2 in set(range(10)) - set(result):
+            assert_equals(0, matrix[v1, v2])
+
+def test_strongly_connected_components():
+    nodes = 10
+    matrix = np.random.randint(2, size=(nodes,nodes))
+    components = ana.strongly_connected_components(matrix)
+    for c1, c2 in zip(components, components[1:]):
+        for v1 in c1:
+            for v2 in c2:
+                assert_true(matrix[v1,v2] == 0 or matrix[v2,v1] == 0)
+    assert_equals(set(range(nodes)), set().union(*components))
