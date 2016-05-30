e1b1abbe95449dbf06d25e70c9a8e59ebec5ca45
= <=>
2016-05-30 13:07:37 +0200

    updated KMeans

--- mcmm/clustering.py
+++ mcmm/clustering.py
@@ -13,7 +13,7 @@ class KMeans(object):
     class providing simple k-Means clustering for (n,d)-shaped 2-dimensional ndarray objects containing float data
     '''
 
-    def __init__(self,data,k,max_iter=100,method='forgy',metric='euclidean'):
+    def __init__(self,data,k,max_iter=25,method='forgy',metric='euclidean'):
         '''
         input
         data: (n,d)-shaped 2-dimensional ndarray objects containing float data
@@ -24,71 +24,107 @@ class KMeans(object):
         self.k = k
         self.max_iter = max_iter
         self.data = data
-        self.method=method
-        self.metric=metric
+        self.method = 'forgy'
+        self.metric = 'euclidean'
         self._cluster_centers = None
         self._cluster_labels = None
 
     @property
-    def cluster_centres(self):
-        if cluster_centres is None:
-            cluster_centers = self.fit()[0]
-            cluster_labels = self.fit()[1]
+    def cluster_centers(self):
+        if self.cluster_centers is None:
+            self.cluster_centers = self.fit()[0]
+            self.cluster_labels = self.fit()[1]
         return self._cluster_centers
+    @cluster_centers.setter
+    def cluster_centers(self,value):
+        self.cluster_centers = value
 
     @property
-    def cluster_assignment(self):
-        if cluster_assigntment is None:
-            cluster_centers = self.fit()[0]
-            cluster_labels = self.fit()[1]
-        return self._cluster_assignment
+    def cluster_labels(self):
+        if self.cluster_labels is None:
+            self.cluster_centers = self.fit()[0]
+            self.cluster_labels = self.fit()[1]
+        return self._cluster_labels
+    @cluster_labels.setter
+    def cluster_labels(self,value):
+        self.cluster_labels = value
 
-    def get_cluster_info(data,cluster_centers,metric='euclidean'):
-        '''
-        for (n,d)-shaped float data and given centroids, returns the corresponding cluster centers
 
-        input
-        data: (n,d) ndarray
-        cluster_centers: (k,d) ndarray
-        metric: metric parameters used as in scipy.spatial.distance.cdist. uses euclidean metric as default.
+    def fit(self):
+        '''
+        '''
+        if self.method == 'forgy':
+            cluster_centers = forgy_centers(self.data,self.k)
 
-        output
-        cluster_labels: (d,1) vector containing the corresponding cluster centers of each of the data rows
-        cluster_dist (d,1) vector containing squared distance of data observation to corresponding cluster centroid
-        '''
-        distance_matrix = distance.cdist(data,cluster_centers,metric)
-        cluster_labels = np.argmin(distance_matrix,axis=1)
-        cluster_dist = np.min(distance_matrix)
-        return cluster_labels, cluster_dist
+        counter = 0
 
-    def wcss(cluster_labels,cluster_dist):
-        '''
-        returns within-cluster-sum-of-squares in a (k,1) shaped vector for each cluster
-        '''
-        wcss_vec = []
-        for i in range(self.k):
-            ss = cluster_dist[cluster_labels == i]
-            wcss_vec.append(sum(ss**2))
-        return wcss_vec
+        while counter < self.max_iter:
+            cluster_labels, cluster_dist = get_cluster_info(data,cluster_centers,metric=self.metric)
+            cluster_centers = set_new_cluster_centers(data,cluster_labels,self.k)
+            counter = counter+1
+            print counter
 
-    def forgy_centers(data):
-        '''
-        returns k randomly chosen cluster centers from data
-        '''
-        return sample(data,self.k)
+        return cluster_centers,cluster_labels
 
-    def optimize_centroid(cluster_points):
-        '''
-        for a given set of observations in one cluster, compute and return a new centroid
-        '''
-        vecsum = np.sum(cluster_points,axis=0)
-        centroid = vecsum/cluster_points.shape[0]
-        return centroid
 
-    def fit():
-        '''
-        '''
-        #TODO actual clustering loop
+#--------------
+#global functions
+#--------------
+
+
+def get_cluster_info(data,cluster_centers,metric='euclidean'):
+    '''
+    for (n,d)-shaped float data and given centroids, returns the corresponding cluster centers
+
+    input
+    data: (n,d) ndarray
+    cluster_centers: (k,d) ndarray
+    metric: metric parameters used as in scipy.spatial.distance.cdist. uses euclidean metric as default.
+
+    output
+    cluster_labels: (d,1) vector containing the corresponding cluster centers of each of the data rows
+    cluster_dist (d,1) vector containing squared distance of data observation to corresponding cluster centroid
+    '''
+    distance_matrix = distance.cdist(data,cluster_centers,metric)
+    cluster_labels = np.argmin(distance_matrix,axis=1)
+    cluster_dist = np.min(distance_matrix)
+    return cluster_labels, cluster_dist
+
+def wcss(cluster_labels,cluster_dist,k):
+    '''
+    returns within-cluster-sum-of-squares in a (k,1) shaped vector for each cluster
+    '''
+    wcss_vec = []
+    for i in range(k):
+        ss = cluster_dist[cluster_labels == i]
+        wcss_vec.append(sum(ss**2))
+    return wcss_vec
+
+def forgy_centers(data,k):
+    '''
+    returns k randomly chosen cluster centers from data
+    '''
+    return sample(data,k)
+
+def optimize_centroid(cluster_points):
+    '''
+    for a given set of observations in one cluster, compute and return a new centroid
+    '''
+    vecsum = np.sum(cluster_points,axis=0)
+    centroid = vecsum/cluster_points.shape[0]
+    return centroid
+
+def set_new_cluster_centers(data,cluster_labels,k):
+    '''
+    for given data and clusterlabeling, construct new centers for each cluster
+    '''
+    center_list = []
+    for i in range(k):
+        new_center = optimize_centroid(data[cluster_labels == i,:])
+        center_list.append(new_center)
+
+    return np.vstack(center_list)
+
 
 
 
