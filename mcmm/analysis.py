Fixed wrong normalization of stationary distribution. Also removed redundant assert.

mcmm/analysis.py
@@ -99,7 +99,8 @@ class MarkovStateModel:
         norms = [np.absolute(v) for v in eigenvalues]
         i = np.argmax(norms)
         assert(np.isclose(eigenvalues[i], 1))
-        return eigenvectors[:,i]
+        v = eigenvectors[:,i]
+        return v/sum(v)
     
     def forward_committors(self, A, B):
         """Returns the vector of forward commitors from A to B"""
@@ -157,8 +158,6 @@ def strongly_connected_components(adjacency_matrix):
     for node in nodes:
         if not flags[node]:
             node_list += depth_first_search(adjacency_matrix, node, flags)
-    for n in nodes:
-        assert(flags[n])
     flags = [False] * len(nodes)
     components = []
     for node in reversed(node_list):

test/test_analysis.py
@@ -15,13 +15,15 @@ def test_find_stationary_distribution():
     msm = ana.MarkovStateModel(matrix)
     distrib = msm.stationary_distribution
     np.testing.assert_array_almost_equal(np.dot(matrix.T, distrib), distrib)
-    np.testing.assert_almost_equal(np.linalg.norm(distrib), 1)
+    np.testing.assert_almost_equal(np.sum(distrib), 1)
+
 
 def test_find_stationary_distribution_raises():
     matrix = np.zeros(shape=(3, 3))
     msm = ana.MarkovStateModel(matrix)
     with assert_raises(ana.InvalidOperation):
         distrib = msm.stationary_distribution
+
 
 def test_depth_first_search():
     nodes = 10
@@ -34,6 +36,7 @@ def test_depth_first_search():
         for v2 in set(range(10)) - set(result):
             assert_equals(0, matrix[v1, v2])
 
+
 def test_strongly_connected_components():
     nodes = 10
     matrix = np.random.randint(2, size=(nodes,nodes))
@@ -43,7 +46,8 @@ def test_strongly_connected_components():
             for v2 in c2:
                 assert_true(matrix[v1,v2] == 0 or matrix[v2,v1] == 0)
     assert_equals(set(range(nodes)), set().union(*components))
-    
+
+
 def test_forward_commitor():
     matrix = np.random.rand(4,4)
     matrix[0,1] = matrix[0,2] + matrix[0,3]
@@ -61,6 +65,7 @@ def test_reversible():
     ])
     msm = ana.MarkovStateModel(matrix)
     assert_true(msm.is_reversible)
+
 
 def test_not_reversible():
     matrix = np.array([