82193c193c00e86b256433a328496f7b71c2d48d
Malte Renken <malte.renken@wwu.de>
2016-06-09 15:02:05 +0200

    Added trivial transition matrix estimation.

--- mcmm/estimation.py
+++ mcmm/estimation.py
@@ -2,5 +2,16 @@ r"""
 This module should handle the transition counting and transition matrix estimation.
 """
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+__metaclass__ = type
+
+import numpy as np
+
+def transition_matrix_from_count_matrix(count_matrix):
+    row_sums = count_matrix.sum(axis=1)
+    count_matrix /= row_sums[:, np.newaxis]
+    return count_matrix
 
 
+def reversible_transition_matrix_from_count_matrix(count_matrix):
+    raise NotImplementedError()
