06a91437d710566aa6ae02d2fb2e0837a1e14b68
Malte Renken <malte.renken@wwu.de>
2016-06-10 11:41:36 +0200

    Wrap estimation functions into new class Estimator.

--- mcmm/estimation.py
+++ mcmm/estimation.py
@@ -7,37 +7,66 @@ __metaclass__ = type
 
 import numpy as np
 
-def count_matrix_from_cluster_labels_using_sliding_window(cluster_labels, lag_time, window_shift, num_cluster):
-    """Computes the count matrix based on the cluster labels. The method is the sliding window approach.
-    
-    Parameters:
-    cluster_labels: 1-dimensional numpy.ndarray, every entry gives the cluster number of the element
-    lag_time: number, size of the window
-    window_shift: number of trajectory indices by which windows is shifted	
-    num_cluster: number of clusters
-    """
-    num_states = cluster_labels.shape[0]
-    count_matrix = np.zeros((num_cluster,num_cluster))
-    for s in range (0, num_states-lag_time, window_shift):
-        count_matrix[cluster_labels[s],cluster_labels[s + lag_time]] +=1    
-    return count_matrix	
+class Estimator:
+    def __init__(self, trajectory, lag_time=1, window_shift=1):
+        """Constructor.
 
-def transition_matrix_from_count_matrix(count_matrix):
-    return make_stochastic(count_matrix)
+        Arguments:
+        trajectory: 1-dimensional np.ndarray
+            Entry #i contains the cluster number at time i. Cluster numbers should be non-negative integers.
+        lag_time: int, default=1
+            Lag time of the markov process
+        window_shift: int, default=1
+            Window shifting distance of the estimator. Value should be in range 1 to lag_time.
+        """
+        self._trajectory = trajectory
+        self._lag_time = lag_time
+        self._window_shift = window_shift
+        self._num_clusters = np.max(trajectory)+1
+
+        self._count_matrix = None
+        self._transition_matrix = None
+        self._reversible_transition_matrix = None
+
+    @property
+    def count_matrix(self):
+        if self._count_matrix is None:
+            self._count_matrix = self._compute_count_matrix()
+        return self._count_matrix
 
 
-def reversible_transition_matrix_from_count_matrix(count_matrix):
-    matrix = transition_matrix_from_count_matrix(count_matrix)
-    matrix_next = np.zeros(matrix.shape)
-    while not np.allclose(matrix, matrix_next, rtol=0.01):
-        for i,j in np.ndindex(*count_matrix.shape):
-            d_i = count_matrix[i,:].sum() / matrix[i,:].sum()
-            d_j = count_matrix[j,:].sum() / matrix[j,:].sum()
-            matrix_next[i,j] = (count_matrix[i,j] + count_matrix[j,i])/(d_i + d_j)
-        matrix, matrix_next = matrix_next, matrix
-    return make_stochastic(matrix)
+    def _compute_count_matrix(self):
+        """Computes the count matrix based on the cluster labels. The method is the sliding window approach."""
+        num_states = self._trajectory.shape[0]
+        count_matrix = np.zeros((self._num_clusters,self._num_clusters))
+        for s in range(0, num_states-self._lag_time, self._window_shift):
+            count_matrix[self._trajectory[s], self._trajectory[s + self._lag_time]] += 1
+        return count_matrix
+
+    @property
+    def transition_matrix(self):
+        if self._transition_matrix  is None:
+            self._transition_matrix = make_stochastic(self.count_matrix)
+        return self._transition_matrix
+
+    @property
+    def reversible_transition_matrix(self):
+        if self._reversible_transition_matrix is None:
+            self._reversible_transition_matrix = self._compute_reversible_transition_matrix()
+        return self._reversible_transition_matrix
+
+    def _compute_reversible_transition_matrix(self):
+        matrix = self.transition_matrix
+        matrix_next = np.zeros(matrix.shape)
+        while not np.allclose(matrix, matrix_next, rtol=0.01):
+            for i,j in np.ndindex(*self.count_matrix.shape):
+                d_i = self.count_matrix[i,:].sum() / matrix[i,:].sum()
+                d_j = self.count_matrix[j,:].sum() / matrix[j,:].sum()
+                matrix_next[i,j] = (self.count_matrix[i,j] + self.count_matrix[j,i])/(d_i + d_j)
+            matrix, matrix_next = matrix_next, matrix
+        return make_stochastic(matrix)
+
 
 def make_stochastic(matrix):
     row_sums = matrix.sum(axis=1)
     return matrix / row_sums[:, np.newaxis]
-
--- test/test_estimation.py
+++ test/test_estimation.py
@@ -8,25 +8,26 @@ from numpy.testing import assert_array_equal
 def test_compute_counting_matrix():
     #check simple example with 10 states and 3 clusters
     a = np.array([1,2,1,2,2,1,1,1,2,1])
-    matrix = est.count_matrix_from_cluster_labels_using_sliding_window(a, 3, 2, 3)
+    matrix = est.Estimator(a, 3, 2).count_matrix
     assert_array_equal(np.array([[0,0,0],[0,2,1],[0,1,0]]), matrix)
     #check simple example with 10 states and 3 clusters, different lag_time
     a = np.array([1,2,1,2,2,1,1,1,2,1])
-    matrix = est.count_matrix_from_cluster_labels_using_sliding_window(a, 1, 1, 3)
+    matrix = est.Estimator(a, 1, 1).count_matrix
     assert_array_equal(np.array([[0,0,0],[0,2,3],[0,3,1]]), matrix)
     #check simple example with 10 states and 2 clusters
     a = np.array([0,1,0,1,1,0,0,0,1,0])
-    matrix = est.count_matrix_from_cluster_labels_using_sliding_window(a, 1, 1, 2)
+    matrix = est.Estimator(a, 1, 1).count_matrix
     assert_array_equal(np.array([[2,3],[3,1]]), matrix)
     #check simple example with 10 states and 2 clusters, maximal lag_time
     a = np.array([0,1,0,1,1,0,0,0,1,0])
-    matrix = est.count_matrix_from_cluster_labels_using_sliding_window(a, 9, 1, 2)
+    matrix = est.Estimator(a, 9, 1).count_matrix
     assert_array_equal(np.array([[1,0],[0,0]]), matrix)
     #check simple example with 10 states and 2 clusters, high window shift
     a = np.array([0,1,0,1,1,0,0,0,1,0])
-    matrix = est.count_matrix_from_cluster_labels_using_sliding_window(a, 4, 6, 2)
+    matrix = est.Estimator(a, 4, 6).count_matrix
     assert_array_equal(np.array([[0,1],[0,0]]), matrix)
-    
+
+
 def test_simple_markov():
     """This test generates data based on a small Markov model
     and tests the resulting transition matrix against the original transition matrix
@@ -43,25 +44,21 @@ def test_simple_markov():
             cluster_labels[i] = 1
         else:
             cluster_labels[i] = 2
-    count_matrix = est.count_matrix_from_cluster_labels_using_sliding_window(cluster_labels,1,1,3)
-    transition_matrix = est.transition_matrix_from_count_matrix(count_matrix)
-    assert_equals(int(50*np.linalg.norm(transition_matrix - A)),0)
+    estimator = est.Estimator(cluster_labels, 1, 1)
+    assert_equals(int(50*np.linalg.norm(estimator.transition_matrix - A)),0)
+
 
 def test_transition_matrix():
-    count_matrix = np.random.randint(0, 1000, (10, 10))
-    print(count_matrix)
-    transition_matrix = est.transition_matrix_from_count_matrix(count_matrix)
-    print(transition_matrix)
+    clusters = 10
+    traj = np.random.randint(0, clusters, 50)
+    transition_matrix = est.Estimator(traj, 1, 1).transition_matrix
     row_sums = transition_matrix.sum(axis=1)
     np.testing.assert_allclose(row_sums, 1)
-    for i in range(len(count_matrix)):
-        row_sum = sum(count_matrix[i,:])
-        for j in range(len(count_matrix)):
-            np.testing.assert_allclose(transition_matrix[i,j], count_matrix[i,j]/row_sum)
+
 
 def test_transition_matrix_reversible():
-    count_matrix = np.random.randint(0, 1000, (4, 4))
-    transition_matrix = est.reversible_transition_matrix_from_count_matrix(count_matrix)
+    traj = np.random.randint(0, 10, 50)
+    transition_matrix = est.Estimator(traj, 1, 1).reversible_transition_matrix
     msm = ana.MarkovStateModel(transition_matrix)
     assert_true(msm.is_reversible)
 
