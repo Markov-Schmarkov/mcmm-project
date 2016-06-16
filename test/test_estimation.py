228735e5d9a1bcc7d57ff4e70e45ddd825fe060c
lukas-polthier <info@lukas-polthier.de>
2016-06-16 15:59:25 +0200

    test for clustering->estimation

--- test/test_estimation.py
+++ test/test_estimation.py
@@ -1,4 +1,4 @@
-from mcmm import estimation as est, analysis as ana
+from mcmm import estimation as est, analysis as ana, clustering as cl
 import numpy as np
 import random
 import unittest
@@ -46,6 +46,71 @@ def test_simple_markov():
             cluster_labels[i] = 2
     estimator = est.Estimator(cluster_labels, 1, 1)
     np.testing.assert_allclose(estimator.transition_matrix, A, atol=0.05, rtol=0.1)
+    
+def test_clustering_estimation_simple_markov():
+    """This test generates data based on a transition matrix. Then we do clustering.
+    Then we check if the estimated matrix behaves as expected.
+    """
+    A = np.array([[0.5, 0.4, 0.1, 0],[0.2, 0.8, 0, 0],[0,0.05,0.25,0.7],[0,0,0.75,0.25]])
+    for i, row in enumerate(A):
+        A[i] = row/sum(row)
+    n = 10000
+    states = np.zeros((n,1))
+    states[0,0] = 1
+    noise = 1
+    factor = 0.001
+    for i in range(1,n):
+        zahl = np.random.rand(1)
+        if zahl < A[int(states[i-1,0]),0]:
+            states[i,0] = 0 + factor * np.random.rand() /noise
+        elif zahl < A[int(states[i-1,0]),0] + A[int(states[i-1,0]),1]:
+            states[i,0] = 1 + factor * np.random.rand() /noise
+        elif zahl < A[int(states[i-1,0]),0] + A[int(states[i-1,0]),1] + A[int(states[i-1,0]),2]:
+            states[i,0] = 2 + factor * np.random.rand() /noise
+        else:
+            states[i,0] = 3 + factor * np.random.rand() /noise
+    #do the clustering  
+    clustering = cl.KMeans(states,4,method='kmeans++')
+    cluster_centers = clustering.cluster_centers
+    cluster_labels = clustering.cluster_labels
+    cluster_labels = np.array(cluster_labels)
+    #cluster_labels = np.zeros(n)
+    #for i in range(1,n):
+    #    cluster_labels[i] = cluster_labels[i]//1
+    #do the estimation
+    estimator = est.Estimator(cluster_labels, 1, 1)
+    matrix = estimator.transition_matrix
+    print(A)
+    print(cluster_centers)
+    print(matrix)
+    """
+    Q = np.zeros((4,4))
+    for i in range(0,4):
+        index = np.min(cluster_centers)
+        Q[i,i] = 1
+    Qt = Q
+    print(Q)
+    for i in range(0,4):
+        index = np.argmin(cluster_centers)
+        cluster_centers[index] = cluster_centers[index] + 10
+        P = np.zeros((4,4))
+        for j in range(0,4):
+            P[j,j] = 1
+        if i != index:
+            P[i,i] = 0
+            P[index,index] = 0
+            P[index,i] = 1
+            P[i,index] = 1
+        Q = P@Q
+        Qt = Q@P
+        #print(P@Q)
+        print(cluster_centers)
+        print(index)
+    print(Q)
+    print(estimator.transition_matrix)
+    print(Qt@matrix)
+    assert_array_equal(A, estimator.transition_matrix)
+    """
 
 
 def test_transition_matrix():
