d463f5abb12ad1e211715a762611d6f0a31be1f7
Mattes <mm.91@hotmail.de>
2016-06-17 13:23:27 +0200

    fixed formatting problems

--- test/test_estimation.py
+++ test/test_estimation.py
@@ -50,183 +50,106 @@ def test_simple_markov():
 
 
 def test_clustering_estimation_simple_markov():
-    -    """This test generates data based on a transition matrix. Then we do clustering.
- +    """
-    This
-    test
-    generates
-    randomly
-    perturbed
-    data
-    based
-    on
-    a
-    transition
-    matrix.Then
-    we
-    do
-    clustering.
-
-
-Then
-we
-check if the
-estimated
-matrix
-behaves as expected.
-"""
-A = np.array([[0.5, 0.4, 0.1, 0],[0.2, 0.8, 0, 0],[0,0.05,0.25,0.7],[0,0,0.75,0.25]])
-@@ -57,60 +57,96 @@ def test_clustering_estimation_simple_markov():
-n = 10000
-states = np.zeros((n,1))
-states[0,0] = 1
--    noise = 1
-factor = 0.001
-for i in range(1,n):
-    zahl = np.random.rand(1)
-    if zahl < A[int(states[i-1,0]),0]:
--            states[i,0] = 0 + factor * np.random.rand() /noise
-+            states[i,0] = 0 + factor * np.random.rand()
-    elif zahl < A[int(states[i-1,0]),0] + A[int(states[i-1,0]),1]:
--            states[i,0] = 1 + factor * np.random.rand() /noise
-+            states[i,0] = 1 + factor * np.random.rand()
-    elif zahl < A[int(states[i-1,0]),0] + A[int(states[i-1,0]),1] + A[int(states[i-1,0]),2]:
--            states[i,0] = 2 + factor * np.random.rand() /noise
-+            states[i,0] = 2 + factor * np.random.rand()
-    else:
--            states[i,0] = 3 + factor * np.random.rand() /noise
-+            states[i,0] = 3 + factor * np.random.rand()
-#do the clustering
-clustering = cl.KMeans(states,4,method='kmeans++')
-cluster_centers = clustering.cluster_centers
-cluster_labels = clustering.cluster_labels
-cluster_labels = np.array(cluster_labels)
--    #cluster_labels = np.zeros(n)
--    #for i in range(1,n):
--    #    cluster_labels[i] = cluster_labels[i]//1
-#do the estimation
-estimator = est.Estimator(cluster_labels, 1, 1)
-matrix = estimator.transition_matrix
--    print(A)
--    print(cluster_centers)
--    print(matrix)
--    """
--    Q = np.zeros((4, 4))
--
-for i in range(0, 4):
-    -        index = np.min(cluster_centers)
--        Q[i, i] = 1
-+    Q = np.identity(4)
-Qt = Q
--
-print(Q)
-for i in range(0, 4):
-    -        index = np.argmin(cluster_centers)
--        cluster_centers[index] = cluster_centers[index] + 10
--        P = np.zeros((4, 4))
--
-for j in range(0, 4):
-    -            P[j, j] = 1
--
-if i != index:
-    -            P[i, i] = 0
-+        index = np.argmax(cluster_centers)
-+        cluster_centers[index] = cluster_centers[index] - 10
-+        P = np.identity(4)
-+  # permute row and column index with 3-i
-+
-if 3 - i != index:
-    +            P[3 - i, 3 - i] = 0
-P[index, index] = 0
--            P[index, i] = 1
--            P[i, index] = 1
--        Q = P @ Q
--        Qt = Q @ P
--  # print(P@Q)
--
-print(cluster_centers)
--
-print(index)
--
-print(Q)
--
-print(estimator.transition_matrix)
--
-print(Qt @ matrix)
--    assert_array_equal(A, estimator.transition_matrix)
-+            P[index, 3 - i] = 1
-+            P[3 - i, index] = 1
-+        z = cluster_centers[3 - i, 0]
-+        cluster_centers[3 - i] = cluster_centers[index, 0]
-+        cluster_centers[index, 0] = z
-+        Q = P.dot(Q)
-+        Qt = Qt.dot(P)
-+    matrix = Q.dot(matrix).dot(Qt)
-+    np.testing.assert_allclose(matrix, A, atol=0.05, rtol=0.1)
-+
-+
+    """This test generates randomly perturbed data based on a transition matrix. Then we do clustering.
+    Then we check if the estimated matrix behaves as expected.
+    """
+    A = np.array([[0.5, 0.4, 0.1, 0], [0.2, 0.8, 0, 0], [0, 0.05, 0.25, 0.7], [0, 0, 0.75, 0.25]])
+    for i, row in enumerate(A):
+        A[i] = row / sum(row)
+    n = 10000
+    states = np.zeros((n, 1))
+    states[0, 0] = 1
+    factor = 0.001
+    for i in range(1, n):
+        zahl = np.random.rand(1)
+        if zahl < A[int(states[i - 1, 0]), 0]:
+            states[i, 0] = 0 + factor * np.random.rand()
+        elif zahl < A[int(states[i - 1, 0]), 0] + A[int(states[i - 1, 0]), 1]:
+            states[i, 0] = 1 + factor * np.random.rand()
+        elif zahl < A[int(states[i - 1, 0]), 0] + A[int(states[i - 1, 0]), 1] + A[int(states[i - 1, 0]), 2]:
+            states[i, 0] = 2 + factor * np.random.rand()
+        else:
+            states[i, 0] = 3 + factor * np.random.rand()
+    # do the clustering
+    clustering = cl.KMeans(states, 4, method='kmeans++')
+    cluster_centers = clustering.cluster_centers
+    cluster_labels = clustering.cluster_labels
+    cluster_labels = np.array(cluster_labels)
+    # do the estimation
+    estimator = est.Estimator(cluster_labels, 1, 1)
+    matrix = estimator.transition_matrix
+    Q = np.identity(4)
+    Qt = Q
+    for i in range(0, 4):
+        index = np.argmax(cluster_centers)
+        cluster_centers[index] = cluster_centers[index] - 10
+        P = np.identity(4)
+        # permute row and column index with 3-i
+        if 3 - i != index:
+            P[3 - i, 3 - i] = 0
+            P[index, index] = 0
+            P[index, 3 - i] = 1
+            P[3 - i, index] = 1
+        z = cluster_centers[3 - i, 0]
+        cluster_centers[3 - i] = cluster_centers[index, 0]
+        cluster_centers[index, 0] = z
+        Q = P.dot(Q)
+        Qt = Qt.dot(P)
+    matrix = Q.dot(matrix).dot(Qt)
+    np.testing.assert_allclose(matrix, A, atol=0.05, rtol=0.1)
 
 
 def test_clustering_estimation_bigger_markov():
-    +    """This test generates randomly perturbed data based on a transition matrix. Then we do clustering.
- +    Then we check if the estimated matrix behaves as expected.
- +    Just as test_clustering_estimation_simple_markov(), but with an arbitrary bigger random matrix
-      """
-
-
-+    m = 10  # size of original transition matrix, free to choose
-+    A = np.random.rand(m, m) + 0.001
-+
-for i, row in enumerate(A):
-    +        A[i] = row / sum(row)
-+    n = 100000  # number of random evaluations of the Markov process
-+    states = np.zeros((n, 1))
-+    states[0, 0] = 1
-+    factor = 0.001
-+
-for i in range(1, n):
-    +        check = 0
-+        summe = 0
-+        zahl = np.random.rand(1)
-+
-while check < m:
-    +            summe = summe + A[int(states[i - 1, 0]), check]
-+
-if zahl < summe:
-    +                states[i, 0] = check + factor * np.random.rand()
-+                check = m + 1
-+            check = check + 1
-+  # do the clustering
-+    clustering = cl.KMeans(states, m, method='kmeans++')
-+    cluster_centers = clustering.cluster_centers
-+    cluster_labels = clustering.cluster_labels
-+    cluster_labels = np.array(cluster_labels)
-+  # do the estimation
-+    estimator = est.Estimator(cluster_labels, 1, 1)
-+    matrix = estimator.transition_matrix
-+    Q = np.identity(m)
-+    Qt = Q
-+
-for i in range(0, m):
-    +        index = np.argmax(cluster_centers)
-+        cluster_centers[index] = cluster_centers[index] - 10
-+        P = np.identity(m)
-+  # permute row and column index with m-1-i
-+
-if m - 1 - i != index:
-    +            P[m - 1 - i, m - 1 - i] = 0
-+            P[index, index] = 0
-+            P[index, m - 1 - i] = 1
-+            P[m - 1 - i, index] = 1
-+        z = cluster_centers[m - 1 - i, 0]
-+        cluster_centers[m - 1 - i] = cluster_centers[index, 0]
-+        cluster_centers[index, 0] = z
-+        Q = P.dot(Q)
-+        Qt = Qt.dot(P)
-+    matrix = Q.dot(matrix).dot(Qt)
-+
-print(np.linalg.norm(A - matrix))
-+    np.testing.assert_allclose(matrix, A, atol=0.05, rtol=0.1)
+    """This test generates randomly perturbed data based on a transition matrix. Then we do clustering.
+    Then we check if the estimated matrix behaves as expected.
+    Just as test_clustering_estimation_simple_markov(), but with an arbitrary bigger random matrix
+    """
+    m = 10  # size of original transition matrix, free to choose
+    A = np.random.rand(m, m) + 0.001
+    for i, row in enumerate(A):
+        A[i] = row / sum(row)
+    n = 100000  # number of random evaluations of the Markov process
+    states = np.zeros((n, 1))
+    states[0, 0] = 1
+    factor = 0.001
+    for i in range(1, n):
+        check = 0
+        summe = 0
+        zahl = np.random.rand(1)
+        while check < m:
+            summe = summe + A[int(states[i - 1, 0]), check]
+            if zahl < summe:
+                states[i, 0] = check + factor * np.random.rand()
+                check = m + 1
+            check = check + 1
+    # do the clustering
+    clustering = cl.KMeans(states, m, method='kmeans++')
+    cluster_centers = clustering.cluster_centers
+    cluster_labels = clustering.cluster_labels
+    cluster_labels = np.array(cluster_labels)
+    # do the estimation
+    estimator = est.Estimator(cluster_labels, 1, 1)
+    matrix = estimator.transition_matrix
+    Q = np.identity(m)
+    Qt = Q
+    for i in range(0, m):
+        index = np.argmax(cluster_centers)
+        cluster_centers[index] = cluster_centers[index] - 10
+        P = np.identity(m)
+        # permute row and column index with m-1-i
+        if m - 1 - i != index:
+            P[m - 1 - i, m - 1 - i] = 0
+            P[index, index] = 0
+            P[index, m - 1 - i] = 1
+            P[m - 1 - i, index] = 1
+        z = cluster_centers[m - 1 - i, 0]
+        cluster_centers[m - 1 - i] = cluster_centers[index, 0]
+        cluster_centers[index, 0] = z
+        Q = P.dot(Q)
+        Qt = Qt.dot(P)
+    matrix = Q.dot(matrix).dot(Qt)
+    print(np.linalg.norm(A - matrix))
+    np.testing.assert_allclose(matrix, A, atol=0.05, rtol=0.1)
 
 
 def test_transition_matrix():
