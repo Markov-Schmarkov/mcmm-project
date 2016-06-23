0a7267240fbd0cf6975219530bad21152a8495df
lukas-polthier <info@lukas-polthier.de>
2016-06-23 15:38:38 +0200

    Add clustering test using example

--- test/test_clustering.py
+++ test/test_clustering.py
@@ -6,6 +6,7 @@ import random
 import unittest
 from nose.tools import assert_true, assert_false, assert_equals, assert_raises
 from numpy.testing import assert_array_equal
+from mcmm import example as ex
 
 
 def test_find_cluster_centers():
@@ -47,6 +48,7 @@ def test_find_cluster_centers():
         
 def test_find_cluster_centers_R2():
     """This test should check if the cluster_centers are reasonable
+    We test clustering in R^2
     """
     n = 50 #number of data points in x- and y-direction
     k = 5 #number of cluster_centers in x- and y-direction when generating data
@@ -85,6 +87,7 @@ def test_find_cluster_centers_R2():
     
     plt.scatter(cluster_centers[:,0],cluster_centers[:,1],c='r')
     
+    #check if cluster_centers are reasonable
     for i in range(0,k):
         for j in range(0,k):
             check = 0
@@ -92,3 +95,37 @@ def test_find_cluster_centers_R2():
                 if np.linalg.norm([i,j] - cluster_centers[l,:]) < 0.2:
                     check = 1
             assert_equals(check,1)
+
+def test_find_cluster_centers_example1():
+    """This test uses the build in example class to test the clustering
+    In the end we check if the error (sum of distance of all points to nearest cluster center)
+    is smaller than expected
+    """
+    n = 5000 #number of data points
+    k = 15 #number of cluster_centers
+    factor = 0.25 #how much is the data perturbed
+    x = ex.generate_test_data(n,1)
+    data = x[0]
+    #fig,ax = plt.subplots(ncols=2,nrows=1)
+    plt.scatter(data[:,0],data[:,1])
+    
+    clustering = cl.KMeans(data,k,method='kmeans++')
+    cluster_centers = clustering.cluster_centers
+    cluster_labels = clustering.cluster_labels
+    
+    errorNew = 0
+    for i in range(0,n):
+        dist = n
+        for l in range(0,k):
+            distNew = np.linalg.norm(data[i,:] - cluster_centers[l,:])
+            if dist > distNew:
+                dist = distNew
+        errorNew = errorNew + dist
+    print(errorNew/n)
+    print(4/k)
+    check = 0
+    if errorNew/n < 4/k:
+        check = 1
+    assert_equals(check,1)
+
+    plt.scatter(cluster_centers[:,0],cluster_centers[:,1],c='r')
