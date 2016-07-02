cb6db12b1981725801ce2734ed5d2004b3b031d9
Mattes <mm.91@hotmail.de>
2016-07-02 15:29:50 +0200

    added numba as requirement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author='',
     author_email='',
     packages=['mcmm'],
-    install_requires=['numpy', 'msmtools>=1.0', 'matplotlib', 'scipy', 'pandas'],
+    install_requires=['numpy', 'msmtools>=1.0', 'matplotlib', 'scipy', 'pandas','numba'],
     tests_require=['nose'],
     test_suite='nose.collector'
 )
