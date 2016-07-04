83c03fbca95c84e59af01d332295a94ebe5630ca
Malte Renken <malte.renken@fu-berlin.de>
2016-07-04 20:20:10 +0200

    Unify code for left and right eigenvectors and fix right eigenvectors.

--- mcmm/analysis.py
+++ mcmm/analysis.py
@@ -37,6 +37,7 @@ class MarkovStateModel:
         self._is_aperiodic = None
         self._eigenvalues = None
         self._left_eigenvectors = None
+        self._right_eigenvectors = None
         self._communication_classes = None
 
     @property
@@ -132,29 +133,49 @@ class MarkovStateModel:
        
 
     def left_eigenvectors(self, k=None):
-        """Computes the first k eigenvectors for largest eigenvalues
+        """Computes the first k left eigenvectors for largest eigenvalues
         
         Arguments:
         k: int
             How many eigenvectors should be returned. Defaults to None, meaning all.
         
-        Returns: [pandas.Series]    
+        Returns: pandas.DataFrame
+            DataFrame containing the eigenvectors as columns
         """
-        eigenvalues, eigenvectors = self.left_eigen
         if k is None:
-            k = len(eigenvalues)
-        output = [None] * k
-        for i in range(k):
-            jj = eigenvalues.argmax()
-            output[i] = eigenvectors.loc[:,jj]
-            eigenvalues[jj] = np.nan
-        return output
+            k = len(self.transition_matrix)
+        return self.left_eigen[1][:,:k]
     
+    def right_eigenvectors(self, k=None):
+        """Computes the first k right eigenvectors for largest eigenvalues
+        
+        Arguments:
+        k: int
+            How many eigenvectors should be returned. Defaults to None, meaning all.
+        
+        Returns: pandas.DataFrame
+            DataFrame containing the eigenvectors as columns
+        """
+        if k is None:
+            k = len(self.transition_matrix)
+        return self.right_eigen[1][:,:k]
     
     @property
     def is_reversible(self):
         """Whether the markov chain is reversible"""
         return np.allclose(self.backward_transition_matrix, self.transition_matrix)
+    
+    def _right_eigen(self, matrix):
+        eigenvalues, eigenvectors = np.linalg.eig(matrix)
+        # sort by eigenvalues descending:
+        eigenvalues, eigenvectors = zip(*(
+            sorted(zip(eigenvalues, eigenvectors.T), key=lambda x: np.real(x[0]), reverse=True)
+        ))
+        eigenvectors = pd.concat([
+            pd.Series(x, index=matrix.index)
+            for x in eigenvectors
+        ], axis=1)
+        return (eigenvalues, eigenvectors)
 
     @property
     def left_eigen(self):
@@ -164,11 +185,7 @@ class MarkovStateModel:
             where eigenvalues[i] corresponds to eigenvectors[:,i]
         """
         if self._left_eigenvectors is None:
-            self._eigenvalues, self._left_eigenvectors = np.linalg.eig(self.transition_matrix.T)
-            self._left_eigenvectors = pd.DataFrame([
-                pd.Series(x, index=self.transition_matrix.index)
-                for x in self._left_eigenvectors
-            ])
+            self._eigenvalues, self._left_eigenvectors = self._right_eigen(self.transition_matrix.T)
         return (self._eigenvalues, self._left_eigenvectors)
 
     @property
@@ -179,11 +196,7 @@ class MarkovStateModel:
             where eigenvalues[i] corresponds to eigenvectors[:,i]
         """
         if self._right_eigenvectors is None:
-            self._eigenvalues, self._right_eigenvectors = np.linalg.eig(self.transition_matrix)
-            self._right_eigenvectors = pd.DataFrame([
-                pd.Series(x, index=self.transition_matrix.index)
-                for x in self._left_eigenvectors
-            ])
+            self._eigenvalues, self._left_eigenvectors = self._right_eigen(self.transition_matrix)
         return (self._eigenvalues, self._right_eigenvectors)
             
     def _find_stationary_distribution(self):
