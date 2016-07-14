376ba0b7e76ff6d389bf93188d348138205434bc
Malte Renken <malte.renken@wwu.de>
2016-07-14 14:48:18 +0200

    Add py2 compability.

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+__metaclass__ = type
+
 from mcmm import analysis as ana, estimation as est, clustering as cl
 import numpy as np
 import pandas as pd
